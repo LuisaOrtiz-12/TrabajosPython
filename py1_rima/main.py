83795f304e536a5841af9567e0ccb68995081ec5
LUISA FERNANDA FERNANDA <>
2021-04-21 22:54:18 +0000

    Main

---
+++ py1_rima/main.py
@@ -0,0 +1,12 @@
+archivo = open('palabras500.csv', encoding = "utf-8")
+palabra = archivo.readlines()
+archivo.close
+
+def rima(tex):
+    for tex in palabra[i]:
+      if palabra[-1:-2] = tex:
+         print(palabra[i])
+      i = i+1
+      
+rima('an')
+
