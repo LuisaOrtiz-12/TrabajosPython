5f3389ab8f6c11f4f1bd548442e3445fd32b854e
LUISA FERNANDA FERNANDA <>
2021-04-22 19:06:44 +0000

    Python

--- py1_rima/main.py
+++ py1_rima/main.py
@@ -3,10 +3,12 @@ palabra = archivo.readlines()
 archivo.close
 
 def rima(tex):
-    for tex in palabra[i]:
-      if palabra[-1:-2] = tex:
-         print(palabra[i])
-      i = i+1
-      
+    for i in palabra:
+       if i[-3:] == tex:
+        print(i) 
+       else:
+        print('No se encuentran palabras en el archivo')
+            
+
 rima('an')
 
