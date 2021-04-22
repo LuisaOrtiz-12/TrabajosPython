4499beef488235a0badf9edd9477bbfebe8f3ef6
LUISA FERNANDA FERNANDA <>
2021-04-22 22:36:34 +0000

    Main

--- py1_rima/main.py
+++ py1_rima/main.py
@@ -2,12 +2,15 @@ archivo = open('palabras500.csv', encoding = "utf-8")
 palabra = archivo.readlines()
 archivo.close
 
+dat = []
+
 def rima(tex):
-    for i in palabra:
-       if i[-3:] == tex:
-        print(i) 
-       else:
-        print('No se encuentran palabras en el archivo')
+  for i in palabra:
+    dat = i[-3:]
+    if dat == tex:
+      print(dat) 
+    else:
+      print('No se encuentran palabras en el archivo')
             
 rima('an')
 
