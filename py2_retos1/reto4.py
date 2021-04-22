8c1053328027bf4a418feb786e0b07ac5225e226
LUISA FERNANDA FERNANDA <>
2021-04-22 19:59:24 +0000

    Reto 7(SM)

--- py2_retos1/reto4.py
+++ py2_retos1/reto4.py
@@ -1,6 +1,12 @@
 s = 8
 t = 3
 
+x = int(input("¿Cuál es el primer número? "))
+y = int(input("¿Cuál es el segundo número? "))
+
+print(x)
+print(y)
+
 if s < t:
   print(s, ' es menor que ', t)
 elif s == t:
