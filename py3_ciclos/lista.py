187e6d9b75d7d86a8aba594aef502bfd24a99224
LUISA FERNANDA FERNANDA <81333478+LuisaOrtiz-12@users.noreply.github.com>
2021-09-04 00:48:21 +0000

    EjercicioSumatoria2

---
+++ py3_ciclos/lista.py
@@ -0,0 +1,14 @@
+def lista(A,B):
+ n = len(A)//2
+ C = []
+ for i in range(n):
+   d = pow(A[i+1], 2)
+   e = d * B[2*i]
+   f = e + B[n+i]
+   C.append(f)
+
+ print(C)
+
+A = [4,6,8,2]
+B = [2,1,0,-1]
+lista(A,B)
