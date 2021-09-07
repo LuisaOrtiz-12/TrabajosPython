ccc437d8deac646c78a47b469413ff699bae29c0
LUISAFERNANDA14 <>
2021-09-07 00:03:41 +0000

    comprension

--- py3_ciclos/lista.py
+++ py3_ciclos/lista.py
@@ -1,14 +1,14 @@
 def lista(A,B):
  n = len(A)//2
  C = []
- for i in range(n+1):
+ for i in range(n):
    d = pow(A[i+1], 2)
    e = d * B[2*i]
    f = e + B[n+i]
    C.append(f)
 
- print(C)
+ return C
 
 A = [4,6,8,2]
 B = [2,1,0,-1]
-lista(A,B)
+print(lista(A,B))
--- py3_ciclos/sumatoria.py
+++ py3_ciclos/sumatoria.py
@@ -1,15 +1,16 @@
 def sumatoria(A,B,C):
+ i = -1
  n = len(A)
  acumulador = 0
- for i in range(n+1):
+ for i in range(n):
    d = A[i] * B[i]
    e = d + C[i]
    acumulador = acumulador + e
 
  resultado = acumulador + pow(n, 2)
- print(resultado)
+ return resultado
 
 A = [4,6,8]
 B = [2,2,2]
 C = [1,2,3]
-sumatoria(A,B,C)
+print(sumatoria(A,B,C))
